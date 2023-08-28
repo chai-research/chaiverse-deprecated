8d9479380e9d63f1d0a57a4be32f7addd62446fd
Firstname Lastname <name@seamlessml.com>
2023-08-27 18:47:11 -0700

    .

--- setup.py
+++ setup.py
@@ -14,10 +14,12 @@ setup(
     author_email='hello@chai-research.com',
     version="0.1",
     description="Chaiverse",
-    packages=find_packages(),
     install_requires=requirements,
+    include_package_data=True,
     long_description=long_description,
     license='MIT',
     package_dir={"": "src"},
+    packages=find_packages(where="src"),
     long_description_content_type='text/markdown',
 )
+
---
+++ src/chaiverse/__init__.py
@@ -0,0 +1,1 @@
+from .core import LLaMA7b
